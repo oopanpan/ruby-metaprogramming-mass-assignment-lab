b9621a44fc7ef7b50f690915f988a9f30eaedc78
oopanpan <oopanpan@gmail.com>
2021-01-21 14:23:52 -0800

    commit tree 6b511e8b8cba87e1206b2ed0b5b82320f91a410b

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-  #your code here
+  
+  def initialize(good_stuff)
+    good_stuff.each{|attributes, data|
+      self.class.attr_accessor(attributes)
+      self.send(("#{attributes}="), data)
+    }
+  end
 end
